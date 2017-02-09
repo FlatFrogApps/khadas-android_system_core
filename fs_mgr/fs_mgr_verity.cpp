b557e43ed7eb34f2cf3b19db21456af14ff85dbe
Xindong Xu <xindong.xu@amlogic.com>
2017-02-09 14:19:42 +0800

    PD#136759: fs_mgr_verity.cpp: use android-verity instead of verity
    Change-Id: I810a93c3fd4e48f770f7384ba113980b526f248c

--- fs_mgr/fs_mgr_verity.cpp
+++ fs_mgr/fs_mgr_verity.cpp
@@ -312,7 +312,7 @@ static int load_verity_table(struct dm_ioctl *io, char *name, uint64_t device_si
     tgt->status = 0;
     tgt->sector_start = 0;
     tgt->length = device_size / 512;
-    strcpy(tgt->target_type, "verity");
+    strcpy(tgt->target_type, "android-verity");
 
     // build the verity params
     verity_params = buffer + sizeof(struct dm_ioctl) + sizeof(struct dm_target_spec);
